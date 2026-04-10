ec63fc1d485e6e59bc7b0e9a53b433ed7791d2ff
jw2384 <jessicawang4763@gmail.com>
2026-04-10 01:01:56 +0100

    Implement generate_raft_motion_vectors and generate_raft_motion_vectors_zarr

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,7 +10,8 @@ from pathlib import Path
 import sys
 import torch
 import torch.nn.functional as F
-from torchvision.io import decode_image
+from torchvision.io import decode_image, ImageReadMode
+from torchvision.models.optical_flow import raft_large, Raft_Large_Weights
 from torchvision.utils import save_image
 from tqdm import tqdm
 
@@ -251,7 +252,7 @@ def generate_zarr(input_folder_path: Path, zarr_output_path: Path, channels: int
         data[i] = image
 
 
-def generate_vr_data_zarr(input_root_path: Path, output_root_path: Path):
+def generate_vr_data_zarr(input_root_path: Path, output_root_path: Path) -> None:
     for directory_path, _, filenames in os.walk(input_root_path):
         directory_path = Path(directory_path)
         if any(filename.endswith((".png", ".exr")) for filename in filenames):
@@ -270,22 +271,87 @@ def generate_vr_data_zarr(input_root_path: Path, output_root_path: Path):
             generate_zarr(directory_path, output_path, channels)
 
 
-def rename_subdirectories(path: Path):
-    for i in range(60):
-        old_name = f"{i:04d}"
-        new_name = f"{i + 60:04d}"
-        old_path = path / old_name
-        new_path = path / new_name
-        os.rename(old_path, new_path)
-        print(f"Renamed {old_name} -> {new_name}")
+def generate_raft_motion_vectors(parent_path: Path) -> None:
+    device = (
+        torch.accelerator.current_accelerator().type
+        if torch.accelerator.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    # Use the RAFT model
+    raft_weights = Raft_Large_Weights.C_T_SKHT_V2
+    raft_model = raft_large(weights=raft_weights, progress=False).to(device)
+
+    for param in raft_model.parameters():
+        param.requires_grad = False
+
+    raft_model = raft_model.eval()
+
+    # Write and save RAFT computed motion vectors
+    frames_path = parent_path / "Colour"
+    motion_vectors_path = parent_path / "MotionVectorRAFT"
+    instances = os.listdir(frames_path)
+    for instance in instances:
+        instance_frames_path = frames_path / instance
+        instance_motion_vectors_path = motion_vectors_path / instance
+        instance_motion_vectors_path.mkdir(parents=True, exist_ok=True)
+
+        prev_frame = None
+
+        frames = os.listdir(instance_frames_path)
+        for frame_num, frame in enumerate(frames):
+            frame_path = instance_frames_path / frame
+            motion_vector_path = instance_motion_vectors_path / f"{frame_num:04d}.exr"
+
+            frame = decode_image(frame_path.resolve(), mode=ImageReadMode.RGB)[0:3].to(device, dtype=torch.float32)
+            frame = 2.0 * (frame / 255.0) - 1.0  # RAFT expects gamma-corrected frames normalised to [-1, 1]
+            frame = frame.unsqueeze(0)
+            _, _, H, W = frame.shape
+
+            if prev_frame is not None:
+                # Compute optical flow with direction prev_frame -> curr_frame
+                optical_flow = raft_model(prev_frame, frame)[-1]
+
+                # Normalise optical flow to [-1, 1] because output is in pixels
+                optical_flow[:, 0] /= W  
+                optical_flow[:, 1] /= H 
+
+                optical_flow = optical_flow.squeeze(0)
+                optical_flow = torch.cat([optical_flow, torch.zeros((1, H, W)).to(device)])
+                optical_flow = optical_flow.permute(1, 2, 0)  # (3, H, W) -> (H, W, 3)
+                optical_flow = optical_flow.cpu().numpy()
+            else:
+                optical_flow = np.zeros((3, H, W), dtype=np.float32)
+                optical_flow = np.transpose(optical_flow, (1, 2, 0))  # (3, H, W) -> (H, W, 3)
+
+            imageio.v3.imwrite(motion_vector_path, optical_flow)
+
+            prev_frame = frame
+
+        print(f"Processed instance {instance}")
+
+
+def generate_raft_motion_vectors_zarr(input_root_path: Path, output_root_path: Path) -> None:
+    for directory_path, _, _ in os.walk(input_root_path):
+        directory_path = Path(directory_path)
+        if directory_path.parent.name == "MotionVectorRAFT":
+            relative_path = os.path.relpath(directory_path, input_root_path)
+            channels = 2
+            output_path = output_root_path / f"{relative_path}.zarr"
+            generate_zarr(directory_path, output_path, channels)
 
 
 if __name__ == "__main__":
-    write_video(
-        input_path=Path("../data/validation_data/VR_mono/FantasticVillage/1440x1600/Enhanced/0000"),
-        output_path=Path("fantastic_village.mp4"),
-        fps=60
+    generate_raft_motion_vectors_zarr(
+        input_root_path=Path("../data/training_data/VR"),
+        output_root_path=Path("../data/training_data/VR_zarr")
     )
+
+    # generate_raft_motion_vectors(parent_path=Path("../data/training_data/VR/FantasticVillage/720x800/MipBiasMinus1Jittered/Left"))
+    # generate_raft_motion_vectors(parent_path=Path("../data/training_data/VR/FantasticVillage/720x800/MipBiasMinus1Jittered/Right"))
+    # generate_raft_motion_vectors(parent_path=Path("../data/validation_data/VR/FantasticVillage/720x800/MipBiasMinus1Jittered/Left"))
+    # generate_raft_motion_vectors(parent_path=Path("../data/validation_data/VR/FantasticVillage/720x800/MipBiasMinus1Jittered/Right"))
 
     # generate_vr_data_zarr(
     #     input_root_path=Path("../data/training_data/VR"),
@@ -315,6 +381,12 @@ if __name__ == "__main__":
     # instance_evaluation_data(folder_path / "1440x1600/Enhanced/Left")
     # instance_evaluation_data(folder_path / "1440x1600/Enhanced/Right")
     # instance_evaluation_data(folder_path / "720x800/MipBiasMinus1Jittered")  # Subsample CameraData
+
+    # write_video(
+    #     input_path=Path("../data/validation_data/VR_mono/FantasticVillage/1440x1600/Enhanced/0000"),
+    #     output_path=Path("fantastic_village.mp4"),
+    #     fps=60
+    # )
 
     # evaluate(
     #     Path("../data/test_data/QRISP/TestSet/SeaPort/540p/MipBiasMinus1/0000"),
